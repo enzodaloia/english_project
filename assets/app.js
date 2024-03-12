9c403c8a3de4b3f4c1069a83ad3bba59408ccf49
Enzo d'aloia-d'ignazio <112956500+enzodaloia@users.noreply.github.com>
2024-03-12 15:10:02 +0100

    implémentation du template dans Data (pour que tu puisses aller le voir)

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,6 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-// import './styles/app.css';
 import './styles/soft-ui-dashboard-tailwind.css';
 import './styles/nucleo-icons.css';
 import './styles/nucleo-svg.css';
