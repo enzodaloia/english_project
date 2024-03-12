9808b637a97544cb182f7babd64719d44ef591e4
Enzo d'aloia-d'ignazio <112956500+enzodaloia@users.noreply.github.com>
2024-03-12 12:03:56 +0100

    import tailwind + premiere page front (test)

--- assets/app.js
+++ assets/app.js
@@ -5,31 +5,3 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
-import './styles/responsive.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'owl.carousel/dist/assets/owl.carousel.css';
-
-
-
-import $ from 'jquery';
-import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
-import 'typeface-roboto';
-import 'owl.carousel';
-$(document).ready(function(){
-    $(".owl-carousel").owlCarousel({
-        loop: true,
-        margin: 0,
-        navText: [],
-        center: true,
-        autoplay: true,
-        autoplayHoverPause: true,
-        responsive: {
-          0: {
-            items: 1
-          },
-          1000: {
-            items: 3
-          }
-        }
-    });
-});
