7c19f0b5e68d8e3875a9eda0fbf565c25722466e
enzodaloia <112956500+enzodaloia@users.noreply.github.com>
2024-03-12 15:48:43 +0100

    toujours pas le ptn de bon template

--- assets/app.js
+++ assets/app.js
@@ -4,7 +4,4 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/soft-ui-dashboard-tailwind.min.css';
-import './styles/nucleo-icons.css';
-import './styles/nucleo-svg.css';
-import './styles/app.css'; // Si vous avez un fichier app.css
+import './styles/app.css';
