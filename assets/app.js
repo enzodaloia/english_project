e44977fe5a42ea646a9bfc1aa77dec11cf96ef1f
Enzo d'aloia-d'ignazio <112956500+enzodaloia@users.noreply.github.com>
2024-03-12 14:21:16 +0100

    importation d'un autre bo qui marche

--- assets/app.js
+++ assets/app.js
@@ -4,4 +4,10 @@
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+// import './styles/app.css';
+import './styles/soft-ui-dashboard-tailwind.css';
+import './styles/nucleo-icons.css';
+import './styles/nucleo-svg.css';
+import './styles/app.css'; // Si vous avez un fichier app.css
+import './img/apple-icon.png';
+import './img/favicon.png';
