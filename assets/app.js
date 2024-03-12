55af3493178ad588ed53aff1cc975b4d8303b972
Enzo d'aloia-d'ignazio <112956500+enzodaloia@users.noreply.github.com>
2024-03-12 11:24:31 +0100

    template

--- assets/app.js
+++ assets/app.js
@@ -5,5 +5,32 @@
  * which should already be in your base.html.twig.
  */
 import './styles/app.css';
+import './styles/responsive.css';
+
+import $ from 'jquery';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap';
+
+import 'typeface-roboto';
+
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        margin: 0,
+        navText: [],
+        center: true,
+        autoplay: true,
+        autoplayHoverPause: true,
+        responsive: {
+          0: {
+            items: 1
+          },
+          1000: {
+            items: 3
+          }
+        }
+    });
+});
