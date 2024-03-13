9573d461dbf4089739b8f12bf3d63b174d003725
Enzo d'aloia-d'ignazio <112956500+enzodaloia@users.noreply.github.com>
2024-03-13 17:30:39 +0100

    back

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -10,9 +10,16 @@ use Symfony\Component\Routing\Attribute\Route;
 class FrontController extends AbstractController
 {
     #[Route('/', name: 'app_front')]
-    public function index(CardRepository $cardRepository): Response
+    public function index(): Response
     {
         return $this->render('front/index.html.twig', [
+        ]);
+    }
+
+    #[Route('/documentation', name: 'app_doc')]
+    public function doc(CardRepository $cardRepository): Response
+    {
+        return $this->render('front/documentation.html.twig', [
             'cards' => $cardRepository->findAll(),
         ]);
     }
